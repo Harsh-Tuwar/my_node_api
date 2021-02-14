3dc604db12a4fcae711886fd773e0e3f39a4321a
Harsh Tuwar <tuwarharsh08@gmail.com>
2021-02-14 16:49:57 -0400

    updates for SavePortfolio

--- controller/MyDivi.ts
+++ controller/MyDivi.ts
@@ -1,3 +1,4 @@
+import shortid from "shortid";
 import { Request, Response } from "express";
 import { fbAdmin } from "../firebase/MyDivi";
 
@@ -18,10 +19,44 @@ const isAuthenticated = async (req: Request) => {
 	return gtg;
 };
 
+// export const LoadRes = async (req: Request, res: Response) => {
+// 	const isAuthed = await isAuthenticated(req);
+
+// 	if (!isAuthed) return res.status(403).json({ data: "Forbidden" });
+
+// 	const { uid } = req.body;
+
+// 	if (!uid) return res.status(400).json({ data: "Unknown User!" });
+
+// 	let portfolios = null;
+
+// 	await fbAdmin.firestore().collection('Portfolio').doc()
+// };
+
 export const SavePortfolio = async (req: Request, res: Response) => {
 	const isAuthed = await isAuthenticated(req);
 
-	if (!isAuthed) return res.status(403).json({ data: "Forbidden " });
+	if (!isAuthed) return res.status(403).json({ data: "Forbidden" });
 
-	return res.status(200).json({ done: 1 });
-}
+	const { l, uid } = req.body;
+
+	if (!uid) return res.status(400).json({ data: "Unknown User!" });
+
+	if (!l) return res.status(400).json({ data: "Missing Label" });
+
+	const user = await fbAdmin.auth().getUser(uid);
+	const savedPortfolio = {
+		uid: uid,
+		label: l,
+		createdAt: new Date().toISOString(),
+		pid: shortid.generate()
+	};
+
+	if (user) {
+		await fbAdmin.firestore().collection('Portfolio').doc().create(savedPortfolio);
+		
+		return res.status(200).json({ done: 1, savedPortfolio });
+	} else {
+		return res.status(400).json({ done: 0, savedPortfolio: null });
+	}
+};
