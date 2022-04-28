e77ea2adcc4d3f160a0cd658fe233ac2bf7780e4
Harsh Tuwar <tuwarharsh08@gmail.com>
2022-04-27 22:15:28 -0400

    updated contactForm.ts file to use more Types

--- controller/ContactForm.ts
+++ controller/ContactForm.ts
@@ -3,10 +3,23 @@ import nodemailer from 'nodemailer';
 import Mail from 'nodemailer/lib/mailer';
 import { config } from '../config';
 
+const TRANSPORT_HOST = 'smtp.gmail.com';
+const TRANSPORT_PORT = 587;
+
+interface TransportAuth {
+	user: string | undefined;
+	pass: string | undefined;
+}
+interface Transport {
+	host: string;
+	port: number;
+	auth: TransportAuth;
+}
+
 export const Send = (req: express.Request, res: express.Response) => {
-	const transport = {
-		host: 'smtp.gmail.com',
-		port: 587,
+	const transport: Transport = {
+		host: TRANSPORT_HOST,
+		port: TRANSPORT_PORT,
 		auth: {
 			user: config.USERNAME,
 			pass: config.PASS
@@ -30,7 +43,7 @@ export const Send = (req: express.Request, res: express.Response) => {
 		text: content
 	}
 
-	transporter.sendMail(mail, (err: any, _: any) => {
+	transporter.sendMail(mail, (err: Error | null, _: any) => {
 		if (err) {
 			res.json({
 				err: err,
