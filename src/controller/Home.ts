b9d259c30b37de50f703ddbc02714e46b1a16285
Harsh Tuwar <tuwarharsh08@gmail.com>
2024-01-20 23:39:17 -0500

    cleanup

--- src/controller/Home.ts
+++ src/controller/Home.ts
@@ -1,5 +1,6 @@
 import express from 'express';
+import { config } from '../config';
 
 export const Init = (_: express.Request, res: express.Response) => {
-	return res.redirect('https://harshtuwar.ml');
+	return res.redirect(config.HARSH_TUWAR_URL ?? 'https://harshtuwar.vercel.app');
 };
--- src/routes/Routes.ts
+++ src/routes/Routes.ts
@@ -1,33 +1,15 @@
 import * as express from 'express';
 import * as Home from '../controller/Home';
-import * as ShortURL from '../controller/ShortURL';
-import * as ContactForm from '../controller/ContactForm';
-import * as YahooScrapper from '../controller/YahooScrapper';
-import * as StartupIdea from '../controller/StartupIdea';
-import * as Quotes from '../controller/Quotes';
 import * as Joke from '../controller/Joke';
-import * as SmartBhangar from '../controller/SmartBhangar';
+import * as StartupIdea from '../controller/StartupIdea';
+import * as ContactForm from '../controller/ContactForm';
 
 export const InitRoutes = (app: express.Express) => {
 	app.get('/', Home.Init);
 
-	app.post('/shortener', ShortURL.GenerateShortURL);
-	app.get('/shortener/:shortUrl', ShortURL.GetShortURL);
-
 	app.post('/send', ContactForm.Send);
 
-	app.post('/inquiry', SmartBhangar.Inquiry);
-	app.post('/order', SmartBhangar.SubmitOrder);
-	app.get('/images', SmartBhangar.GetImages);
-
-	app.post('/scrap/GetData_ByTicker', YahooScrapper.GetData_ByTicker);
-	app.post('/scrap/GetRecomendationsByTicker', YahooScrapper.GetRecomendationsByTicker);
-	app.post('/scrap/GetTrendingSymbols', YahooScrapper.GetTrendingSymbols);
-	app.post('/scrap/TickerAutoComplete', YahooScrapper.TickerAutoComplete);
-
-	app.get('/ideas', StartupIdea.GetIdea);
+	app.get('/idea', StartupIdea.GetIdea);
 	
 	app.get('/jokes', Joke.GetAJoke);
-
-	app.get('/quotes', Quotes.GetQuote);
 }
